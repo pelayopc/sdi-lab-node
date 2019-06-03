fbd49560277eeb6b6f78705bfe187559c3a9502b
pelayo <pelayopc@gmail.com>
2019-06-03 12:21:17 +0200

    8.3 Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public')); 
+var gestorBD = require("./modules/gestorBD.js"); gestorBD.init(app,mongo);
 // Variables 
 app.set('port', 8081); 
 app.set('db','mongodb://admin:9373375-r@tiendamusica-shard-00-00-k9ldl.mongodb.net:27017,tiendamusica-shard-00-01-k9ldl.mongodb.net:27017,tiendamusica-shard-00-02-k9ldl.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority'); 
 //Rutas/controladores por lógica 
-require("./routes/rusuarios.js")(app, swig);  // (app, param1, param2, etc.) 
-require("./routes/rcanciones.js")(app, swig, mongo);  // (app, param1, param2, etc.) 
+require("./routes/rusuarios.js")(app, swig, gestorBD);  // (app, param1, param2, etc.) 
+require("./routes/rcanciones.js")(app, swig, gestorBD);  // (app, param1, param2, etc.) 
  
 // lanzar el servidor 
 
